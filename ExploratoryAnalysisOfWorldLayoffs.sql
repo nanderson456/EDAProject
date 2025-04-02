9cd116495b2dcd453351e3f56df893c9648d84ce
nanderson456 <nicholasanderson12@gmail.com>
2025-04-02 15:35:24 -0400

    Add files via upload

---
+++ ExploratoryAnalysisOfWorldLayoffs.sql
@@ -0,0 +1,144 @@
+-- Exploratory Data Analysis
+
+SELECT *
+FROM layoffs_staging2;
+
+
+SELECT MAX(total_laid_off), MAX(percentage_laid_off)
+FROM layoffs_staging2;
+
+SELECT *
+FROM layoffs_staging2
+WHERE percentage_laid_off = 1
+ORDER BY funds_raised_millions DESC;
+
+
+SELECT company, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC;
+
+
+SELECT MIN(`date`), MAX(`date`)
+FROM layoffs_staging2;
+
+
+SELECT industry, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY industry
+ORDER BY 2 DESC;
+
+
+SELECT country, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY country
+ORDER BY 2 DESC;
+
+
+SELECT *
+FROM layoffs_staging2;
+
+
+SELECT YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY YEAR(`date`)
+ORDER BY 1 DESC;
+
+
+SELECT stage, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY stage
+ORDER BY 2 DESC;
+
+
+SELECT company, SUM(percentage_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC;
+
+-- Amount laid off every month
+SELECT SUBSTRING(`date`,1,7) AS `MONTH`, SUM(total_laid_off)
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY `MONTH`
+ORDER BY 1 ASC
+;
+
+-- Rolling total off amount laid off by every month
+WITH Rolling_Total AS
+(
+SELECT SUBSTRING(`date`,1,7) AS `MONTH`, SUM(total_laid_off) AS total_off
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY `MONTH`
+ORDER BY 1 ASC
+)
+SELECT `MONTH`, total_off,
+ SUM(total_off) OVER (ORDER BY `MONTH`) AS rolling_total
+FROM Rolling_Total;
+
+
+
+SELECT company, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC;
+
+
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+ORDER BY 3 DESC;
+
+
+-- This will bring us the comapnies with the Top 5 highest layoffs of 2020 - 2023 and ranks them 
+WITH Company_Year (company, years, total_laid_off) AS
+(
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+), Company_Year_Rank AS
+(
+SELECT *, DENSE_RANK() OVER (PARTITION BY years ORDER BY total_laid_off DESC) AS Ranking
+FROM Company_Year
+WHERE years IS NOT NULL
+)
+SELECT *
+FROM Company_Year_Rank
+WHERE Ranking <= 5;
+
+
+
+SELECT *
+FROM layoffs_staging2;
+
+SELECT DISTINCT stage
+FROM layoffs_staging2;
+
+-- This will bring you the total amount laid off for every stage from 2020 - 2023 in Descending Order
+WITH Stage_Year (stage, years, total_laid_off) AS
+(
+SELECT stage, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY stage, YEAR(`date`)
+)
+SELECT *
+FROM Stage_Year
+ORDER BY total_laid_off DESC
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
